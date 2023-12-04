f5fdd5bea24c483994081be72f357c64dce14812
ls3648 <146682844+ls3648@users.noreply.github.com>
2023-12-04 18:56:18 -0500

    Added modal

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,29 @@
 // var sectionTags = document.querySelector(".myClass");
 var mainNav = document.querySelector("#mainNav");
 var buttonContainer = document.querySelector("#buttons-container");
-var burgerBtn = document.querySelector("#burger-container button");
+var burgerBtn = document.querySelector("#burger-container");
 
-// this can only be true or false since it's a boolean
+// initial condition statement, this can only be true or false since it's a boolean
 var canYouSeeMainNav = false;
 
 function showNav(){
-    // console.log("button clicked");
-    if(canYouSeeMainNav == false){
+    // if the initial condition is met, do something
+    if(canYouSeeMainNav == false) {
         console.log("show menu");
         // this will add the class of showMainNav to the id mainNav
         mainNav.classList.add("showMainNav");
-
+        // this will add the class of showButton to the id buttonContainer
         buttonContainer.classList.add("showButton");
-        
+        // sets boolean to true
         canYouSeeMainNav=true;
     }
     else{
         console.log("hide menu");
-        // this will remove the class of showMainNav to the id mainNav
+        // this will remove the class of showMainNav from the id mainNav
         mainNav.classList.remove("showMainNav");
-
+        // this will remove the class of showButton from the id buttonContainer
         buttonContainer.classList.remove("showButton");
-
+        // sets boolean back to initial state of false
         canYouSeeMainNav=false;
     }
 }
@@ -41,12 +41,23 @@ burgerBtn.addEventListener("click",showNav);
 ------------------- */
 
 var signInBtn = document.querySelector("#sign-in-btn");
+var signInContainer = document.querySelector("#sign-in-container");
+var closeSignInModalBtn = document.querySelector("#close-sign-in-modal-btn");
 
 function showSignInModal() {
     console.log("show modal now");
+    signInContainer.classList.add("show-Sign-In-Container");
+    mainNav.classList.remove("showMainNav");
+    buttonContainer.classList.remove("showButton");
+    canYouSeeMainNav=false;
+}
+function hideSignInModal() {
+    console.log("hide modal now");
+    signInContainer.classList.remove("show-Sign-In-Container");
 }
 
 signInBtn.addEventListener("click", showSignInModal);
+closeSignInModalBtn.addEventListener("click", hideSignInModal);
 
 
 
